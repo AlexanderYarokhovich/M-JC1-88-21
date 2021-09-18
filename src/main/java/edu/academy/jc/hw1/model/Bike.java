f90a5b7e33864b343dfb76c0d593e75ac85f5eff
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2021-09-18 19:00:44 +0300

    Add home work 1

---
+++ src/main/java/edu/academy/jc/hw1/model/Bike.java
@@ -0,0 +1,22 @@
+package edu.academy.jc.hw1.model;
+
+public class Bike {
+    private String color;
+    private int speed;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
